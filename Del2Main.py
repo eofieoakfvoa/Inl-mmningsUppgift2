08284507798367f77411f09921f3db605cfefddc
meow <114763852+eofieoakfvoa@users.noreply.github.com>
2025-03-30 17:31:57 +0200

    :cat:

--- Del2Main.py
+++ Del2Main.py
@@ -5,6 +5,7 @@ def Main():
     running = True
     previousUsedFile = None
     while running:
+        print(previousUsedFile)
         print("Skriv en av följande eller inget för att gå ut  \n",
               "[Write] för att skriva till en textfil \n",
               "[Overwrite] för att skriva över en textfil \n",
@@ -18,18 +19,28 @@ def Main():
         elif userInput == "read":
             pass
         elif userInput == "create":
-            CreateFile()
-        
+            success = CreateFile()
+            if success[0] == True:
+                previousUsedFile = success[1]
         elif userInput == "":
-            pass
+            running = False
         else:
             print("kontrollera att du skrev rätt, du skrev ", userInput)
 def CreateFile():
     print("Skriv först ett namn till filen")
     name = input()
-    print("Skriv nu path till vart du vill att den ska vara eller [Current] för att få den i samma map som denna är i")
+    print("Skriv nu en path till vart du vill att den ska vara eller [Current] för att få den i samma map som denna är i")
     choice = input()
+    Success = None
     if choice.lower() == "current":
-        Del2Modul.CreateTextFile(os.path.abspath(os.getcwd()), name)
+        Success = Del2Modul.CreateTextFile(os.path.abspath(os.getcwd()), name)
+
+    if not Success:
+        print("Det gick inte att skapa en fil")
+    elif Success:
+        print("Det gick att skapa en fil! :D")
+        return Success
+
+
 
 Main()
--- Del2Modul.py
+++ Del2Modul.py
@@ -1,14 +1,16 @@
 import os
 def CreateTextFile(path, name):
+    pathAddedWithName = path + "/" + name + ".txt"
     if not DirectoryExists(path):
         print("Finns ingen map i denna path : ", path)
         return False
-    pathAddedWithName = path + "/" + name + ".txt"
-    print(pathAddedWithName)
+    if FileExists(pathAddedWithName):
+        print("Det finns redan en fil med det namnet i vald mapp : ", pathAddedWithName)
+        return False
     open(pathAddedWithName, "x")
+    return True, pathAddedWithName
 def WriteToTextFile(path, string):
     file = open(path, "a")
-
     file.close()
 
 def OverwriteTextFile(path, string):
@@ -17,5 +19,8 @@ def ReadFromTextFile(path):
     file = open(path, "r")
     return file.read()
 
+#dessa är mäst till för att det ser finare ut där de används
 def DirectoryExists(path):
     return os.path.isdir(path)
+def FileExists(path):
+    return os.path.exists(path)
