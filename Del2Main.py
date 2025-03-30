5d28845b7ffe53eb87968e4886ce7d84da154070
meow <114763852+eofieoakfvoa@users.noreply.github.com>
2025-03-30 18:18:31 +0200

    :cat:

--- Del2Main.py
+++ Del2Main.py
@@ -4,43 +4,69 @@ import os
 def Main():
     running = True
     previousUsedFile = None
-    while running:
-        print(previousUsedFile)
+    while running: #sålänge running är true kommer programmet att repeata
         print("Skriv en av följande eller inget för att gå ut  \n",
               "[Write] för att skriva till en textfil \n",
               "[Overwrite] för att skriva över en textfil \n",
               "[Read] för att läsa från en text fil \n",
+              "[Create] för att skapa en text fil\n",
               "[] för att stänga programmet")
         userInput = input().lower()
         if userInput == "write":
-            pass
+            success = WriteToFile(previousUsedFile, "a") # "a" = append, jag tycker att det är lättare för användarn att write är inte att man skriver över allt
+            if success != None: #kanske borde gjort såhär när man skapar en fil (jag skrev detta efter jag skrev create metoden)
+                previousUsedFile = success
         elif userInput == "overwrite":
-            pass
+            success = WriteToFile(previousUsedFile, "w") # "w" = write, som betyder att den skriver över allt
+            if success != None: #kanske borde gjort såhär när man skapar en fil (jag skrev detta efter jag skrev create metoden)
+                previousUsedFile = success
         elif userInput == "read":
-            pass
+            ReadFromFile(previousUsedFile)
         elif userInput == "create":
-            success = CreateFile()
-            if success[0] == True:
+            success = CreateFile() 
+            print(success)
+            if success != False: #kanske inte är det bästa sättet att göra detta men eftersom ifall den är false så har den bara 1 value och inte 2 som true har, så skulle success[0] == true, gå på grund av false och detta funkar
                 previousUsedFile = success[1]
         elif userInput == "":
+            print("stänger nu programmet")
             running = False
         else:
             print("kontrollera att du skrev rätt, du skrev ", userInput)
+            
 def CreateFile():
     print("Skriv först ett namn till filen")
     name = input()
     print("Skriv nu en path till vart du vill att den ska vara eller [Current] för att få den i samma map som denna är i")
     choice = input()
     Success = None
-    if choice.lower() == "current":
+    if choice.lower() == "current": #lower() för att t.ex Current ska fungera eller cURRENT, jag sätter det inte i input() delen eftersom jag vet inte ifall paths är case sensitive, därför gör jag det härh
         Success = Del2Modul.CreateTextFile(os.path.abspath(os.getcwd()), name)
-
+    else:
+        Success = Del2Modul.CreateTextFile(choice, name)
     if not Success:
         print("Det gick inte att skapa en fil")
+        return False
     elif Success:
         print("Det gick att skapa en fil! :D")
         return Success
 
-
-
+def WriteToFile(previousUsedFile, appendorwrite):
+    print("Skriv en path till en text fil eller skriv [Current] för att använda senast använd fil i detta program som är : ", previousUsedFile) #previousUsedFile blir "None" ifall den inte har en value än
+    choice = input()
+    print("Skriv nu vad som ska skrivas i filen")
+    text = input()
+    if choice.lower() == "current":
+        choice = previousUsedFile
+    Success = Del2Modul.WriteToTextFile(choice, text, appendorwrite)
+    if Success == True:
+        return choice
+    return None
+def ReadFromFile(previousUsedFile):
+    print("Skriv en fil path att läsa från eller skriv [Current] för att använda senast använd fil i detta program som är : ", previousUsedFile)
+    choice = input()
+    if choice.lower() == "current":
+        choice = previousUsedFile
+    Success = Del2Modul.ReadFromTextFile(choice)
+    if Success != False:
+        print("Läser från filen\n",Success, "\n ")
 Main()
--- Del2Modul.py
+++ Del2Modul.py
@@ -1,4 +1,5 @@
 import os
+
 def CreateTextFile(path, name):
     pathAddedWithName = path + "/" + name + ".txt"
     if not DirectoryExists(path):
@@ -7,19 +8,26 @@ def CreateTextFile(path, name):
     if FileExists(pathAddedWithName):
         print("Det finns redan en fil med det namnet i vald mapp : ", pathAddedWithName)
         return False
-    open(pathAddedWithName, "x")
+    open(pathAddedWithName, "x") #x skapar fil, tror den delen av open() liksom bestämmer permissions
     return True, pathAddedWithName
-def WriteToTextFile(path, string):
-    file = open(path, "a")
-    file.close()
 
-def OverwriteTextFile(path, string):
-    pass
+def WriteToTextFile(path, string, writeorappend):
+    if not FileExists(path) or DirectoryExists(path): #detta kollar så att det inte är en mapp och att path leder någon stans, jag vet inte ifall man kan kolla ifall det är en .txt fil, man skulle typ kunna göra en for loop som börjar från slutet och kollar att det innehåler t, x, t
+        print("Filen existerar inte eller så skrev du en mapp") 
+        return False
+    file = open(path, writeorappend) #eftersom jag implimenterar writeorappend delen och det inte är usern som gör det så borde det inte riskera att man gör något fel
+    file.write(string)
+    file.close() # vet inte ifall detta behövs men gör det för det stog så i en av källorna
+    return True
+
 def ReadFromTextFile(path):
-    file = open(path, "r")
+    if not FileExists(path) or DirectoryExists(path): #detta kollar så att det inte är en mapp och att path leder någon stans, jag vet inte ifall man kan kolla ifall det är en .txt fil, man skulle typ kunna göra en for loop som börjar från slutet och kollar att det innehåler t, x, t
+        print("Filen existerar inte eller så skrev du en mapp") 
+        return False
+    file = open(path, "r") #r står för read
     return file.read()
 
-#dessa är mäst till för att det ser finare ut där de används
+#dessa är mest till för att det ser finare ut där de används
 def DirectoryExists(path):
     return os.path.isdir(path)
 def FileExists(path):
